6b6e6893405e35772c7015b7a32d32704b844094
Denet <the.net.wo@gmail.com>
2018-11-14 21:00:21 +0000

    Test my actions

--- app/reducers/__tests__/reducer.test.js
+++ app/reducers/__tests__/reducer.test.js
@@ -11,8 +11,16 @@ describe('Recipe Reducers', () => {
     name: 'Pizza',
     ingredients: ['cheese', 'tomato']
   };
-  
+
   it('Should add recipe to state', () => {
-    expect(recipeReducer([], {type: ADD_RECIPE, recipe})).toEqual([recipe]);
+    expect(recipeReducer([], { type: ADD_RECIPE, recipe })).toEqual([recipe]);
+  });
+
+  it('Should delete recipe from state', () => {
+    const recipes = [recipe, recipe];
+
+    expect(recipeReducer(recipes, { type: DELETE_RECIPE, index: 1 })).toEqual([recipe]);
+
+    expect(recipeReducer(recipes, { type: DELETE_RECIPE, index: 10 })).toEqual(recipes);
   });
 })
